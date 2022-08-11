8e1153ac8e79131b253e3d79556da4abf8fade51
Benno Richters <bennor@topdesk.com>
2022-08-11 12:29:14 +0200

    migrated from paq to packer

--- init.lua
+++ init.lua
@@ -1,44 +1,49 @@
-require 'paq' {
-  'savq/paq-nvim';
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-  'sainnhe/gruvbox-material';
+  use 'sainnhe/gruvbox-material'
 
-  'phaazon/hop.nvim';
+  use 'phaazon/hop.nvim'
 
-  'nvim-lua/popup.nvim';
-  'nvim-lua/plenary.nvim';
-  'nvim-telescope/telescope.nvim';
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
 
-  'neovim/nvim-lspconfig';
-  'RRethy/vim-illuminate';
+  use 'neovim/nvim-lspconfig'
+  use 'RRethy/vim-illuminate'
 
-  'hrsh7th/cmp-nvim-lsp';
-  'hrsh7th/cmp-buffer';
-  'hrsh7th/cmp-path';
-  'hrsh7th/cmp-cmdline';
-  'hrsh7th/nvim-cmp';
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
 
-  'L3MON4D3/LuaSnip';
-  'saadparwaiz1/cmp_luasnip';
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
 
-  'simrat39/symbols-outline.nvim';
+  use 'simrat39/symbols-outline.nvim'
 
-  'jose-elias-alvarez/nvim-lsp-ts-utils';
+  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
 
-  { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' };
-  'nvim-treesitter/playground';
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
 
-  'tpope/vim-obsession';
-  'tpope/vim-commentary';
+  use 'nvim-treesitter/playground'
 
-  'lfv89/vim-interestingwords';
+  use 'tpope/vim-obsession'
+  use 'tpope/vim-commentary'
 
-  'kyazdani42/nvim-web-devicons';
+  use 'lfv89/vim-interestingwords'
 
-  'ruanyl/coverage.vim';
+  use 'kyazdani42/nvim-web-devicons'
 
-  'davidgranstrom/nvim-markdown-preview';
-}
+  use 'ruanyl/coverage.vim'
+
+  use 'davidgranstrom/nvim-markdown-preview'
+end)
 
 vim.g.mapleader = ' '
 
