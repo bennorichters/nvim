e1b28aa0963b958d4f01727b2925f4d8ee932e8b
Benno Richters <bennor@topdesk.com>
2022-11-24 10:48:11 +0100

    other colorscheme

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use 'Mofiqul/dracula.nvim'
+  use 'savq/melange'
 
   use 'phaazon/hop.nvim'
 
