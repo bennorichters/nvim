e010417a4ef25ac3a624d19160ab2e6f71d7663b
Benno Richters <b.richters@gmail.com>
2022-12-04 12:22:18 +0100

    better snips

--- snippets/rust.lua
+++ snippets/rust.lua
@@ -1,6 +1,23 @@
-return {
-  require("luasnip").snippet(
-    { trig = "prt" },
-    { t("println!(\"{:?}\", ") }
-  ),
-}
+local ls = require("luasnip") --{{{
+local s = ls.s
+local i = ls.i
+local t = ls.t
+
+local snippets, autosnippets = {}, {} --}}}
+
+local pr1 = s('pr1', {
+  t('println!("{:?}", '),
+  i(1, 'v1'),
+  t(');'),
+})
+
+local pr2 = s('pr2', {
+  t('println!("{:?}, {:?}", '),
+  i(1, 'v1, v2'),
+  t(');'),
+})
+
+table.insert(snippets, pr1)
+table.insert(snippets, pr2)
+
+return snippets, autosnippets
