8b9ac3d1431b9ad1211a5fe57890b79c4988b7b0
Benno Richters <b.richters@gmail.com>
2022-12-04 10:31:47 +0100

    clean up

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -1,4 +1,4 @@
-vim.cmd[[
+vim.cmd [[
 " Use Tab to expand and jump through snippets
 imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
 smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
@@ -8,5 +8,4 @@ imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '
 smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 ]]
 
-require("luasnip.loaders.from_lua").load({paths = vim.fn.stdpath("config") .. "/snippets/"})
-
+require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
--- snippets/rust.lua
+++ snippets/rust.lua
@@ -1,7 +1,6 @@
 return {
-  -- A snippet that expands the trigger "hi" into the string "Hello, world!".
   require("luasnip").snippet(
     { trig = "prt" },
-    { t("println!(\"{:?}\", );") }
+    { t("println!(\"{:?}\", ") }
   ),
 }
