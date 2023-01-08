97df79c6c67aa8e8697221376f455a6f4a8577eb
Benno Richters <b.richters@gmail.com>
2023-01-08 16:01:21 +0100

    vim as local var for lua

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,16 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
 lsp.configure('rust_analyzer', {
   settings = {
     ["rust-analyzer"] = {
