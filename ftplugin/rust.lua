bf1f5060284005cdc3e77c6a33c3895b72e630be
Benno Richters <b.richters@gmail.com>
2023-03-12 19:51:18 +0100

    test commands for rust (cargo)

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,5 +1,7 @@
 vim.keymap.set('i', '<F5>', '<ESC>:w<CR>:sp<CR>:term cargo run --bin %:t:r<CR>')
+vim.keymap.set('i', '<F6>', '<ESC>:w<CR>:sp<CR>:term cargo test %:t:r_tests::<CR>')
 vim.keymap.set('n', '<F5>', ':w<CR>:sp<CR>:term cargo run --bin %:t:r<CR>')
+vim.keymap.set('n', '<F6>', ':w<CR>:sp<CR>:term cargo test %:t:r_tests::<CR>')
 
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 vim.keymap.set('n', '<F9>', ':bd!<CR>')
