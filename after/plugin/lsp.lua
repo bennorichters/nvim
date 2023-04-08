b85ed55a59a9ac8cba9eff771fc4efe61ae378a6
Benno Richters <b.richters@gmail.com>
2023-04-08 07:59:06 +0200

    remove path from completion sources

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,7 +6,6 @@ end)
 
 lsp.setup_nvim_cmp({
   sources = {
-    {name = 'path'},
     {name = 'nvim_lsp', keyword_length = 2},
     {name = 'buffer', keyword_length = 3},
     {name = 'luasnip', keyword_length = 2},
--- init.lua
+++ init.lua
@@ -43,7 +43,6 @@ require("lazy").setup({
       { 'hrsh7th/nvim-cmp' }, -- Required
       { 'hrsh7th/cmp-nvim-lsp' }, -- Required
       { 'hrsh7th/cmp-buffer' }, -- Optional
-      { 'hrsh7th/cmp-path' }, -- Optional
       { 'saadparwaiz1/cmp_luasnip' }, -- Optional
       { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
