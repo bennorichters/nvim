16a45daffc25eda8884bf1cab67113bed7c890ae
Benno Richters <b.richters@gmail.com>
2023-04-08 08:06:21 +0200

    mason-lspconfig

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
 
   "simrat39/symbols-outline.nvim",
 
-    {
+  {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     dependencies = {
@@ -38,16 +38,17 @@ require("lazy").setup({
           pcall(vim.cmd, 'MasonUpdate')
         end,
       },
+      { 'williamboman/mason-lspconfig.nvim' },      -- Optional
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' }, -- Required
-      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'hrsh7th/cmp-buffer' }, -- Optional
+      { 'hrsh7th/nvim-cmp' },         -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+      { 'hrsh7th/cmp-buffer' },       -- Optional
       { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-      { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
       -- Snippets
-      { 'L3MON4D3/LuaSnip' }, -- Required
+      { 'L3MON4D3/LuaSnip' },             -- Required
       { 'rafamadriz/friendly-snippets' }, -- Optional
     }
   },
