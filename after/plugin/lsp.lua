13768f2b111e7130f09aa5f35002bb468f518139
Benno Richters <b.richters@gmail.com>
2023-04-09 13:24:12 +0200

    removed deprecated setup_nvim_cmp()

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,25 +1,35 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(_, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
 end)
 
-lsp.setup_nvim_cmp({
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
   sources = {
-    {name = 'nvim_lsp', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-    {name = 'luasnip', keyword_length = 2},
+    { name = 'nvim_lsp', keyword_length = 2 },
+    { name = 'buffer',   keyword_length = 3 },
+    { name = 'luasnip',  keyword_length = 2 },
   }
 })
 
 lsp.configure('lua_ls', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
     }
+  }
 })
 
 lsp.configure('rust_analyzer', {
