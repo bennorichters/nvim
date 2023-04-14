5f19193622b4b847d7a88a560082f5430905d2a2
Benno Richters <bennor@topdesk.com>
2023-04-14 13:00:43 +0200

    keymap for changing kanagawa theme

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,1 +1,15 @@
 vim.cmd("colorscheme kanagawa")
+
+function change_color_theme()
+  current = require('kanagawa')._CURRENT_THEME
+  if (current == 'wave') then
+    vim.cmd("colorscheme kanagawa-dragon")
+  elseif (current == 'dragon') then
+    vim.cmd("colorscheme kanagawa-lotus")
+  else 
+    vim.cmd("colorscheme kanagawa-wave")
+  end
+end
+
+vim.keymap.set('n', '<leader>c', ':lua change_color_theme()<CR>')
+
