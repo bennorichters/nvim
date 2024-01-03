83fe2539b514815e512e05b4a43ca1100391855e
Benno Richters <b.richters@gmail.com>
2024-01-03 20:20:15 +0100

    typo in comment

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,7 +1,7 @@
 ; extends
 
 ; Identifies lines that start and end with a # symbol. Those lines are used
-; as a list of tags (# seperated) in zettelkasten notes. They should not be 
+; as a list of tags (# separated) in zettelkasten notes. They should not be 
 ; spell checked.
 
 (paragraph
