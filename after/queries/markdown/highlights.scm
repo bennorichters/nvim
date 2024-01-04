e9cacbdc304fd095872f0be23876f79560cfb208
Benno Richters <b.richters@gmail.com>
2024-01-04 09:09:09 +0100

    tags are : separated

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,10 +1,10 @@
 ; extends
 
-; Identifies lines that start and end with a # symbol. Those lines are used
-; as a list of tags (# separated) in zettelkasten notes. They should not be 
+; Identifies lines that start and end with a : (colon). Those lines are used
+; as a list of tags (: separated) in zettelkasten notes. They should not be 
 ; spell checked.
 
 (paragraph
-  (inline) @taglist (#match? @taglist "^#.*#$")
+  (inline) @taglist (#match? @taglist "^:.*:$")
 ) @nospell
 
