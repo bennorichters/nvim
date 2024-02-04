be88eb88f40202cf9a123b43d9e1121b74b53e9d
Benno Richters <b.richters@gmail.com>
2024-02-04 09:48:08 +0100

    python3

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,7 +1,7 @@
-vim.keymap.set('i', '<F5>', '<ESC>:w<CR>:sp<CR>:term python %<CR>')
-vim.keymap.set('n', '<F5>', ':w<CR>:sp<CR>:term python %<CR>')
+vim.keymap.set('i', '<F5>', '<ESC>:w<CR>:sp<CR>:term python3 %<CR>')
+vim.keymap.set('n', '<F5>', ':w<CR>:sp<CR>:term python3 %<CR>')
 
-vim.keymap.set('i', '<F6>', '<ESC>:w<CR>:sp<CR>:term python -m unittest<CR>')
-vim.keymap.set('n', '<F6>', ':w<CR>:sp<CR>:term python -m unittest<CR>')
+vim.keymap.set('i', '<F6>', '<ESC>:w<CR>:sp<CR>:term python3 -m unittest<CR>')
+vim.keymap.set('n', '<F6>', ':w<CR>:sp<CR>:term python3 -m unittest<CR>')
 
 vim.keymap.set('n', '<F9>', ':bd!<CR>')
