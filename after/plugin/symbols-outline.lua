3be3c13a8058ec23d93c68debcc59ad9b171c084
Benno Richters <b.richters@gmail.com>
2024-03-13 10:14:52 +0100

    auto command for outline to turn off spell

--- after/plugin/symbols-outline.lua
+++ after/plugin/symbols-outline.lua
@@ -1,2 +1,8 @@
 require("symbols-outline").setup()
 
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+  pattern = { "Outline" },
+  callback = function()
+    vim.opt.spell = false
+  end
+})
