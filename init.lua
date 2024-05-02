5b39c5dff0419256bd1f0932bbe0b99295655f18
Benno Richters <b.richters@gmail.com>
2024-05-02 21:22:45 +0200

    removed rest.nvim

--- init.lua
+++ init.lua
@@ -78,13 +78,5 @@ require("lazy").setup({
       rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
     }
   },
-  {
-    "rest-nvim/rest.nvim",
-    ft = "http",
-    dependencies = { "luarocks.nvim" },
-    config = function()
-      require("rest-nvim").setup()
-    end,
-  },
   { "MunifTanjim/nui.nvim" },
 })
