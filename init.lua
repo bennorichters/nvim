aa97b4820c13e007da5dd6888a59103278278651
Benno Richters <b.richters@gmail.com>
2024-06-25 10:36:52 +0200

    updated Lazy

--- init.lua
+++ init.lua
@@ -2,75 +2,71 @@ vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "rebelot/kanagawa.nvim",
-  "phaazon/hop.nvim",
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  "simrat39/symbols-outline.nvim",
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup()
-    end,
-  },
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x"
-  },
-  "neovim/nvim-lspconfig",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/nvim-cmp",
-  "L3MON4D3/LuaSnip",
-  "rafamadriz/friendly-snippets",
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-  },
-  {
-    "tzachar/local-highlight.nvim",
-    config = function()
-      require("local-highlight").setup()
-    end,
-  },
-  "lfv89/vim-interestingwords",
-  "nvim-tree/nvim-web-devicons",
-  "davidgranstrom/nvim-markdown-preview",
-  {
-    "echasnovski/mini.files",
-    version = "*",
-    config = function()
-      require("mini.files").setup()
-    end,
-  },
+  spec = {
+    "rebelot/kanagawa.nvim",
+    "phaazon/hop.nvim",
+    {
+      'nvim-telescope/telescope.nvim',
+      branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    "simrat39/symbols-outline.nvim",
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        require("mason").setup()
+      end,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+        require("mason-lspconfig").setup()
+      end,
+    },
+    {
+      "VonHeikemen/lsp-zero.nvim",
+      branch = "v3.x"
+    },
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+    },
+    {
+      "tzachar/local-highlight.nvim",
+      config = function()
+        require("local-highlight").setup()
+      end,
+    },
+    "lfv89/vim-interestingwords",
+    "nvim-tree/nvim-web-devicons",
+    "davidgranstrom/nvim-markdown-preview",
+    {
+      "echasnovski/mini.files",
+      version = "*",
+      config = function()
+        require("mini.files").setup()
+      end,
+    },
 
-  {
-    "zk-org/zk-nvim",
-    config = function()
-      require("zk").setup({
-        picker = "telescope",
-      })
-    end
-  },
-  "MunifTanjim/nui.nvim",
+    {
+      "zk-org/zk-nvim",
+      config = function()
+        require("zk").setup({
+          picker = "telescope",
+        })
+      end
+    },
+    "MunifTanjim/nui.nvim",
+  }
 })
